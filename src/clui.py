3118ca3475dfb8acf64a5ad8f7e3ecf6149aad64
David Besau <david.besau@hadiko.de>
2021-07-02 23:21:26 +0200

    Initial code dump

---
+++ src/clui.py
@@ -0,0 +1,20 @@
+import argparse
+import os
+
+OPTION_FILE_NOT_FOUND = "Could not find option file: "
+
+
+def error(message):
+    print("Error, " + message)
+
+
+def option_file_path(path):
+    if path[0] == '/':
+        if not os.path.exists(path):
+            raise argparse.ArgumentTypeError(OPTION_FILE_NOT_FOUND + path)
+        return path
+    else:
+        relative_path = os.getcwd() + '/' + path
+        if not os.path.exists(relative_path):
+            raise argparse.ArgumentTypeError(OPTION_FILE_NOT_FOUND + relative_path)
+        return relative_path
---
+++ src/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import mysql.connector
+
+import clui
+
+PROGRAM_NAME = "redmine-settings-change"
+VERSION = "0.1"
+
+parser = argparse.ArgumentParser(description="Automatically change specific Redmine settings in a MySQL database")
+parser.add_argument("command", metavar="COMMAND", choices=["test"], help="The operation to perform. Choices include "
+                                                                         "%(choices)s.")
+parser.add_argument("--option-file", "-o", type=clui.option_file_path, help="Path to a valid MySQL option file.")
+parser.add_argument("--version", action='version', version=PROGRAM_NAME + " " + VERSION)
+
+
+def get_option_files():
+    possible_option_files = [
+        os.getcwd() + '/my.cnf',
+        os.path.dirname(os.path.abspath(__file__)) + '/my.cnf',
+    ]
+    option_files = []
+    for file in possible_option_files:
+        if os.path.exists(file):
+            option_files.append(file)
+    return option_files
+
+
+def main():
+    args = parser.parse_args()
+
+    if 'option_file' in vars(args) and args.option_file:
+        option_files = [args.option_file]
+    else:
+        option_files = get_option_files()
+        if len(option_files) == 0:
+            clui.error("no valid option file found")
+            exit(1)
+
+    cnx = mysql.connector.connect(option_files=option_files)
+
+    if not cnx.is_connected():
+        clui.error("could not connect to mysql database")
+        exit(1)
+
+    if args.command == "test":
+        print("could connect!")
+
+
+if __name__ == '__main__':
+    main()
