3688bb3c99bdb185ea8cb0afd81ad2819920feed
David Besau <david.besau@hadiko.de>
2021-09-01 15:11:13 +0200

    Add method for changing the number of recently used projects

--- src/redmine.py
+++ src/redmine.py
@@ -3,6 +3,7 @@ import re
 from exception import SettingNotFoundError, IntegerConversionError
 
 RECENTLY_USED_PROJECTS_REGEX = r'^:recently_used_projects: (\d+)$'
+RECENTLY_USED_PROJECTS_REPLACEMENT = ':recently_used_projects: {}'
 
 
 def get_recently_used_projects(user_preferences: str) -> int:
@@ -14,3 +15,8 @@ def get_recently_used_projects(user_preferences: str) -> int:
         return int(match.group(1))
     except ValueError:
         raise IntegerConversionError(match.group(1))
+
+
+def set_recently_used_projects(user_preferences: str, value: int) -> str:
+    pattern = re.compile(RECENTLY_USED_PROJECTS_REGEX, re.MULTILINE)
+    return pattern.sub(RECENTLY_USED_PROJECTS_REPLACEMENT.format(value), user_preferences)
