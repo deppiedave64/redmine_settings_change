22b4d8badb43c89338130ba55f3467775f8e6eee
David Besau <david.besau@protonmail.com>
2021-09-20 22:59:35 +0200

    Rename methods

--- src/redmine.py
+++ src/redmine.py
@@ -6,7 +6,7 @@ RECENTLY_USED_PROJECTS_REGEX = r'^:recently_used_projects: (\d+)$'
 RECENTLY_USED_PROJECTS_REPLACEMENT = ':recently_used_projects: {}'
 
 
-def get_recently_used_projects(user_preferences: str) -> int:
+def get_recently_used_projects_string(user_preferences: str) -> int:
     pattern = re.compile(RECENTLY_USED_PROJECTS_REGEX, re.MULTILINE)
     match = pattern.search(user_preferences)
     if match is None:
@@ -17,6 +17,6 @@ def get_recently_used_projects(user_preferences: str) -> int:
         raise IntegerConversionError(match.group(1))
 
 
-def set_recently_used_projects(user_preferences: str, value: int) -> str:
+def replace_recently_used_projects_string(user_preferences: str, value: int) -> str:
     pattern = re.compile(RECENTLY_USED_PROJECTS_REGEX, re.MULTILINE)
     return pattern.sub(RECENTLY_USED_PROJECTS_REPLACEMENT.format(value), user_preferences)
