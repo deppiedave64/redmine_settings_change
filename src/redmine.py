e09213ca14f01f169f28b2795d886dc37c02f1f8
David Besau <david.besau@protonmail.com>
2021-09-20 23:00:07 +0200

    Add method for changing the recently used projects setting for all users

--- src/redmine.py
+++ src/redmine.py
@@ -1,6 +1,8 @@
 import re
+from mysql.connector.cursor import MySQLCursorAbstract
 
 from exception import SettingNotFoundError, IntegerConversionError
+from db import get_user_list, get_user_preferences, set_user_preferences
 
 RECENTLY_USED_PROJECTS_REGEX = r'^:recently_used_projects: (\d+)$'
 RECENTLY_USED_PROJECTS_REPLACEMENT = ':recently_used_projects: {}'
@@ -20,3 +22,10 @@ def get_recently_used_projects_string(user_preferences: str) -> int:
 def replace_recently_used_projects_string(user_preferences: str, value: int) -> str:
     pattern = re.compile(RECENTLY_USED_PROJECTS_REGEX, re.MULTILINE)
     return pattern.sub(RECENTLY_USED_PROJECTS_REPLACEMENT.format(value), user_preferences)
+
+
+def set_recently_used_projects_for_all_users(cnx: MySQLCursorAbstract, value: int) -> None:
+    users = get_user_list(cnx)
+    for user in users:
+        preferences = get_user_preferences(cnx, user)
+        set_user_preferences(cnx, user, replace_recently_used_projects_string(preferences, value))
